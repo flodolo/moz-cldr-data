18d9f80fe597e1b1dd6dd47adeee9c65b6d560bc
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2018-01-11 10:08:40 +0100

    Compare en-US vs CLDR for languages and territories, plural checks

---
+++ languages_regions.py
@@ -0,0 +1,261 @@
+#! /usr/bin/env python
+
+from glob import glob
+import codecs
+import json
+import locale as locale_module
+import os
+import sys
+
+# You should run this script in a virtualenv, after installing requirements
+# (both Python and Node)
+from compare_locales import parser
+
+def parse_file(file_path, storage):
+    file_extension = os.path.splitext(file_path)[1]
+    file_parser = parser.getParser(file_extension)
+    file_parser.readFile(file_path)
+    try:
+        entities, map = file_parser.parse()
+        for entity in entities:
+            # Ignore Junk
+            if isinstance(entity, parser.Junk):
+                continue
+            storage[unicode(entity)] = entity.val
+    except Exception as e:
+        print('Error parsing file: {}'.format(file_path))
+        print(e)
+
+def main():
+    # Path to this script
+    script_folder = os.path.abspath(os.path.dirname(__file__))
+
+    # Path to folder containing clones from http://hg.mozilla.org/l10n-central
+    l10n_path='/Users/flodolo/mozilla/mercurial/l10n_clones/locales'
+
+    # Path to clone of https://hg.mozilla.org/l10n/gecko-strings
+    en_path='/Users/flodolo/mozilla/mercurial/gecko-strings-quarantine'
+    l10n_path='/Users/flodolo/mozilla/mercurial/l10n_clones/locales/{}'
+
+    # Allow to pipe the output in Bash
+    sys.stdout = codecs.getwriter(locale_module.getpreferredencoding())(sys.stdout)
+
+    # This array is used to map a Mozilla code to CLDR, e.g.
+    # 'es-ES': 'es'
+    locale_mapping = {
+        'bn-BD': 'bn',
+        'en-US': 'en',
+        'es-ES': 'es',
+        'fy-NL': 'fy',
+        'ga-IE': 'ga',
+        'gu-IN': 'gu',
+        'hi-IN': 'hi',
+        'hy-AM': 'hy',
+        'ja-JP-mac': 'ja',
+        'nb-NO': 'nb',
+        'ne-NP': 'ne',
+        'nn-NO': 'nn',
+        'pa-IN': 'pa',
+        'pt-BR': 'pt',
+        'sv-SE': 'sv',
+        'zh-CN': 'zh-Hans',
+        'zh-TW': 'zh-Hant',
+    }
+
+    # This array is used to export comparison data to CSV
+    csv_rows = []
+
+    # Get the list of l10n repositories
+    moz_locales = []
+    for locale_path in glob('{}/*/'.format(l10n_path.replace('/{}', ''))):
+        moz_locales.append(os.path.basename(os.path.normpath(locale_path)))
+    moz_locales.sort()
+
+    # Read languages from en-US
+    moz_languages = {}
+    parse_file(
+        os.path.join(en_path, 'toolkit', 'chrome', 'global', 'languageNames.properties'),
+        moz_languages
+    )
+    # Read languages from CLDR
+    cldr_localenames_path=os.path.join(script_folder, 'node_modules', 'cldr-localenames-full', 'main')
+    with open(os.path.join(cldr_localenames_path, 'en', 'languages.json')) as data_file:
+        json_data = json.load(data_file)
+    cldr_languages = json_data['main']['en']['localeDisplayNames']['languages']
+    # Check for missing languages in CLDR
+    missing_languages = []
+    different_values_lang = []
+    for locale, language in moz_languages.iteritems():
+        if locale not in cldr_languages:
+            missing_languages.append(u'{}: {}'.format(locale, language))
+        elif language != cldr_languages[locale]:
+            different_values_lang.append(u'{}\n  CLDR: {}\n  Mozilla: {}'.format(locale, cldr_languages[locale], language))
+
+    missing_languages.sort()
+    if missing_languages:
+        print('\nMissing language names in CLDR:')
+        print('\n'.join(missing_languages))
+    different_values_lang.sort()
+    if different_values_lang:
+        print('\nDifferent values:')
+        print('\n'.join(different_values_lang))
+
+    # Read regions from en-US
+    moz_regions = {}
+    parse_file(
+        os.path.join(en_path, 'toolkit', 'chrome', 'global', 'regionNames.properties'),
+        moz_regions
+    )
+    # Read regions from CLDR
+    with open(os.path.join(cldr_localenames_path, 'en', 'territories.json')) as data_file:
+        json_data = json.load(data_file)
+    cldr_regions = json_data['main']['en']['localeDisplayNames']['territories']
+    # Check for missing regions in CLDR
+    missing_regions = []
+    different_values_reg = []
+    for region_code, region_name in moz_regions.iteritems():
+        code = region_code.upper()
+        if code not in cldr_regions:
+            missing_regions.append(u'{}: {}'.format(code, region_name))
+        elif region_name != cldr_regions[code]:
+            different_values_reg.append(u'{}\n  CLDR: {}\n  Mozilla: {}'.format(code, cldr_regions[code], region_name))
+
+    missing_regions.sort()
+    if missing_regions:
+        print('\nMissing region names in CLDR:')
+        print('\n'.join(missing_regions))
+    different_values_reg.sort()
+    if different_values_reg:
+        print('\nDifferent values:')
+        print('\n'.join(different_values_reg))
+
+    row = [
+        'en-US (en)',
+        len(moz_languages),
+        len(different_values_lang),
+        round(len(different_values_lang) / float(len(moz_languages)) * 100, 2) if len(moz_languages) > 0 else 0,
+        len(moz_regions),
+        len(different_values_reg),
+        round(len(different_values_reg) / float(len(moz_regions)) * 100, 2) if len(moz_regions) > 0 else 0
+    ]
+    csv_rows.append(','.join(map(str, row)))
+
+    # Check if there are Mozilla locales missing from CLDR
+    cldr_supported_locales = []
+    for locale_path in glob('{}/*/'.format(cldr_localenames_path)):
+        cldr_supported_locales.append(os.path.basename(os.path.normpath(locale_path)))
+    cldr_supported_locales.sort()
+
+    missing_locales = []
+    missing_moz_locales = []
+    # Read CLDR Seed locales from local TXT file
+    seed_locales = []
+    seed_file = open(os.path.join(script_folder, 'data', 'seed_locales.txt'))
+    for line in seed_file:
+        seed_locales.append(line.rstrip())
+    seed_locales.sort()
+
+    for locale in moz_locales:
+        # Remove the region code
+        locale_code = locale.split('-')[0]
+
+        if locale_mapping.get(locale, locale) not in cldr_supported_locales:
+            if locale in seed_locales:
+                missing_locales.append('{} (available in seed)'.format(locale))
+            elif locale_code in seed_locales:
+                missing_locales.append('{} (available in seed as {})'.format(locale, locale_code))
+            else:
+                missing_locales.append(locale)
+        if locale_code not in moz_languages:
+            print locale
+            missing_moz_locales.append(locale)
+    missing_locales.sort()
+    missing_moz_locales.sort()
+    if missing_locales:
+        print('\nLocales not supported by CLDR:')
+        print('\n'.join(missing_locales))
+    if missing_moz_locales:
+        print('\nMissing locales in languageNames.properties:')
+        print('\n'.join(missing_moz_locales))
+
+    # Compare data for all locales
+    for locale in moz_locales:
+        # Ignore locales not available in CLDR
+        if locale_mapping.get(locale, locale) not in cldr_supported_locales:
+            continue
+
+        print('\n-----\nLOCALE: {}'.format(locale))
+
+        # Read languages from Mozilla repository
+        moz_languages = {}
+        different_values_lang = []
+        l10n_file = os.path.join(l10n_path.format(locale), 'toolkit', 'chrome', 'global', 'languageNames.properties')
+        if os.path.isfile(l10n_file):
+            parse_file(l10n_file, moz_languages)
+
+            # Read languages from CLDR
+            cldr_localenames_path=os.path.join(script_folder, 'node_modules', 'cldr-localenames-full', 'main')
+            with open(os.path.join(cldr_localenames_path, locale_mapping.get(locale, locale), 'languages.json')) as data_file:
+                json_data = json.load(data_file)
+            cldr_languages = json_data['main'][locale_mapping.get(locale, locale)]['localeDisplayNames']['languages']
+
+            # Check for different translations
+            for locale_code, language in moz_languages.iteritems():
+                # IMPORTANT:
+                # To reduce the number of differences:
+                # - Case is ignored
+                # - Mozilla strings are trimmed
+                language = language.lower().strip().replace('\u0020', '')
+                cldr_language = cldr_languages[locale_code].lower() if locale_code in cldr_languages else False;
+                if cldr_language and language != cldr_language:
+                    different_values_lang.append(u'{}\n  CLDR: {}\n  Mozilla: {}'.format(locale_code, cldr_language, language))
+            different_values_lang.sort()
+            if different_values_lang:
+                print('\nDifferent values:')
+                print('\n'.join(different_values_lang))
+
+        # Read languages from Mozilla repository
+        moz_regions = {}
+        different_values_reg = []
+        l10n_file = os.path.join(l10n_path.format(locale), 'toolkit', 'chrome', 'global', 'regionNames.properties')
+        if os.path.isfile(l10n_file):
+            parse_file(l10n_file, moz_regions)
+
+            # Read regions from CLDR
+            with open(os.path.join(cldr_localenames_path, locale_mapping.get(locale, locale), 'territories.json')) as data_file:
+                json_data = json.load(data_file)
+            cldr_regions = json_data['main'][locale_mapping.get(locale, locale)]['localeDisplayNames']['territories']
+
+            # Check for different translations
+            for region_code, region_name in moz_regions.iteritems():
+                code = region_code.upper()
+                if code in cldr_regions and region_name != cldr_regions[code]:
+                    different_values_reg.append(u'{}\n  CLDR: {}\n  Mozilla: {}'.format(code, cldr_regions[code], region_name))
+            different_values_reg.sort()
+            if different_values_reg:
+                print('\nDifferent values:')
+                print('\n'.join(different_values_reg))
+
+        # Append data
+        row = [
+            '{} ({})'.format(locale, locale_mapping.get(locale, locale)),
+            len(moz_languages),
+            len(different_values_lang),
+            round(len(different_values_lang) / float(len(moz_languages)) * 100, 2) if len(moz_languages) > 0 else 0,
+            len(moz_regions),
+            len(different_values_reg),
+            round(len(different_values_reg) / float(len(moz_regions)) * 100, 2) if len(moz_regions) > 0 else 0
+        ]
+        csv_rows.append(','.join(map(str, row)))
+
+    # Save .csv data
+    output_file = open('output.csv', 'w')
+    output_file.write(',Region Names,,,Language Names\n')
+    output_file.write('Locale (CLDR),Total,Differences,%,Total,Differences,%\n')
+    csv_rows.sort()
+    for line in csv_rows:
+        output_file.write('{}\n'.format(line))
+    output_file.close()
+
+if __name__ == '__main__':
+    main()
---
+++ plurals.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+
+from glob import glob
+import json
+import os
+import sys
+
+# Path to this script
+script_folder = os.path.abspath(os.path.dirname(__file__))
+
+# Path to local clone of https://github.com/unicode-cldr/cldr-localenames-full
+cldr_path=os.path.join(script_folder, 'node_modules', 'cldr-core', 'supplemental')
+
+# Path to folder containing clones from http://hg.mozilla.org/l10n-central
+l10n_path='/Users/flodolo/mozilla/mercurial/l10n_clones/locales'
+
+# Path to clone of https://hg.mozilla.org/l10n/gecko-strings
+en_path='/Users/flodolo/mozilla/mercurial/gecko-strings-quarantine'
+
+# You should run this script in a virtualenv, after installing requirements
+from compare_locales import plurals as cl_plurals
+from fluent.migrate import cldr
+
+def main():
+    # Get the list of l10n repositories
+    moz_locales = []
+    for locale_path in glob('{}/*/'.format(l10n_path)):
+        moz_locales.append(os.path.basename(os.path.normpath(locale_path)))
+    moz_locales.sort()
+
+    # Read CLDR plurals data
+    with open(os.path.join(cldr_path, 'plurals.json')) as data_file:
+        json_data = json.load(data_file)
+    cldr_plurals = json_data['supplemental']['plurals-type-cardinal']
+
+    for locale in moz_locales:
+        # Check locale in compare-locales
+        if locale not in cl_plurals.CATEGORIES_BY_LOCALE:
+            #print('{} is not supported by compare-locales'.format(locale))
+            continue
+
+        # Check locale in CLDR
+        supported = False
+        if locale in cldr_plurals:
+            supported = True
+        else:
+            # Try removing the region
+            fallback_locale = locale.split('-')[0]
+            if fallback_locale in cldr_plurals:
+                supported = True
+        if not supported:
+            #print('{} is not supported by CLDR'.format(locale))
+            continue
+
+        # Compare the number of forms
+        if cldr.get_plural_categories(locale) != cl_plurals.CATEGORIES_BY_LOCALE[locale]:
+            print('Locale: {}'.format(locale))
+            print('CLDR')
+            print cldr.get_plural_categories(locale)
+            print('compare-locales')
+            print cl_plurals.CATEGORIES_BY_LOCALE[locale]
+
+
+if __name__ == '__main__':
+    main()
